963f4c6c77f32adc34d9e47ed6b61492691b2388
vincentheynderickx <v.heynderickx@gmail.com>
2023-12-12 15:23:02 +0100

    rassemblement des deux codes

--- script.py
+++ script.py
@@ -1,11 +1,24 @@
 import pygame
-import sys
 
-pygame.init()
+def init_game():
+    pygame.init()
+    pygame.display.set_caption("snake")
+    screen.fill( (0, 0, 0) )
+    for k in range (32):
+        for i in range (24):
+            color = (255, 255, 255) 
+            left=40*k
+            if (i%2==0):
+                left+=20
+            top=20*i
+            width=20
+            height=20
+            rect = pygame.Rect(left, top, width, height)
+            pygame.draw.rect(screen, color, rect)
 
-screen = pygame.display.set_mode( (400, 300) )
-pygame.display.set_caption("snake")
 clock = pygame.time.Clock()
+screen = pygame.display.set_mode( (400, 300) )
+init_game()
 
 while True:
     clock.tick(1)
@@ -15,5 +28,4 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 pygame.quit()
-    screen.fill( (0, 255, 0) )
     pygame.display.update()
