015b089208e402d67140427ace6d17c209ece461
vincentheynderickx <v.heynderickx@gmail.com>
2023-12-12 16:01:08 +0100

    mise au propre dans une classe

--- script.py
+++ script.py
@@ -23,44 +23,38 @@ init_board()
 class Snake:
     def __init__(self):
         self.position = [[5,10],[6,10],[7,10]]
+        self.direction = [-1,0]
         self.display()
     def display(self):
         for square in self.position:
             pygame.draw.rect(screen, 'green', pygame.Rect(20*square[0], 20*square[1], 20, 20))
+    def is_movement_possible(self):
+        if (self.position[0][0]==0) and (self.direction==[-1,0]):
+            return(False)
+        if (self.position[0][0]==19) and (self.direction==[1,0]):
+            return(False)
+        if (self.position[0][1]==0) and (self.direction==[0,1]):
+            return(False)
+        if (self.position[0][1]==14) and (self.direction==[0,-1]):
+            return(False)
+        return(True)
+    def move(self):
+        longueur_snake=len(self.position)
+        if not(self.is_movement_possible):
+            pygame.quit()
+        for k in range (1,longueur_snake):
+            self.position[longueur_snake-k]=self.position[longueur_snake-k-1]
+        self.position[0][0]+=self.direction[0]
+        self.position[0][1]+=self.direction[1]
 
 snake = Snake()
 
 while True:
     clock.tick(1)
-    if position
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 pygame.quit()
-            if event.key==pygame.
     pygame.display.update()
-
-def is_movement_possible(direction, position):
-    if (position[0][0]==0) and (direction==[-1,0]):
-        return(False)
-    if (position[0][0]==19) and (direction==[1,0]):
-        return(False)
-    if (position[0][1]==0) and (direction==[0,1]):
-        return(False)
-    if (position[0][1]==14) and (direction==[0,-1]):
-        return(False)
-    return(True)
-
-def move(direction, position):
-    longueur_snake=len(position)
-    if not(is_movement_possible):
-        pygame.quit()
-    for k in range (1,longueur_snake):
-        position[longueur_snake-k]=position[longueur_snake-k-1]
-    position[0][0]+=direction[0]
-    position[0][1]+=direction[1]
-
-
-    
